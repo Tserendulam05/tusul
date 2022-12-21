3a4c607d8ba46efa57c871b44377f6eeea0ef27e
Tserendulam05 <113074600+Tserendulam05@users.noreply.github.com>
2022-12-22 07:42:28 +0800

    Add files via upload

---
+++ web(tusul)/js/script.js
@@ -0,0 +1,7 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick =() =>{
+	navbar.classList.toggle('active');
+}
+let searchFrom = document.querySelector('.search-from')
+document.querySelector('#search-btn').onclick =() =>{
+	navbar.classList.toggle('active');
